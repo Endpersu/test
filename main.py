80fe56d3e197780da2820b177b4dec3c659c6841
Endpersu <67841931+Endpersu@users.noreply.github.com>
2025-02-14 10:02:46 +0000

    aaa

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from loguru import logger
+
+
 def main():
-    pass
+    logger.add("file.log", format ="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+               rotation = "3 days",
+               backtrace = True,
+               diagnose = True)
+    
+    title = input("Введите название новеллы: ")
+    url = input("Введите ссылку на новеллу: ")
+    logger.info(f"Пользователь ввел название {title} новеллы и ссылку {url}.")
 
 
 if __name__ == '__main__':
---
+++ novel.py
@@ -0,0 +1,4 @@
+class Novel:
+    def __init__(self, title):
+        self.title = title
+        self.chapter = {}
